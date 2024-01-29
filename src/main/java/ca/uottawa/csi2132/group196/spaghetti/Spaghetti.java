348e6d7a3bd7877d84ee02a0cba523d6a2b09020
GentlyTech <andyyep@hotmail.com>
2024-01-28 23:01:24 -0500

    Rename project

---
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Spaghetti.java
@@ -0,0 +1,13 @@
+package ca.uottawa.csi2132.group196.spaghetti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spaghetti {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spaghetti.class, args);
+	}
+
+}
---
+++ src/test/java/ca/uottawa/csi2132/group196/spaghetti/SpaghettiTests.java
@@ -0,0 +1,13 @@
+package ca.uottawa.csi2132.group196.spaghetti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaghettiTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
