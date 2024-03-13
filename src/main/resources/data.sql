5f3984c7fbe969f70dfd8f8d5fecf355bb4aaaab
GentlyTech <andyyep@hotmail.com>
2024-03-13 16:33:06 -0400

    Add sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO hotel_chain (chain_name) VALUES ('Hilton');
+INSERT INTO hotel_chain (chain_name) VALUES ('Hyatt');
+INSERT INTO hotel_chain (chain_name) VALUES ('Marriott');
+INSERT INTO hotel_chain (chain_name) VALUES ('Travelodge');
+INSERT INTO hotel_chain (chain_name) VALUES ('Walt Disney');
