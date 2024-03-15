54ad7484c039faa1aca935060c5e47a0272ecd83
GentlyTech <andyyep@hotmail.com>
2024-03-15 16:39:13 -0400

    Split controllers into separate classes

---
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Controllers/HotelChainController.java
@@ -0,0 +1,34 @@
+package ca.uottawa.csi2132.group196.spaghetti.Controllers;
+
+import ca.uottawa.csi2132.group196.spaghetti.Entities.HotelChain;
+import ca.uottawa.csi2132.group196.spaghetti.Repositories.HotelChainRepository;
+import com.google.gson.Gson;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/hotelChain")
+public class HotelChainController {
+
+    private final HotelChainRepository hotelChainRepository;
+
+    public HotelChainController(final HotelChainRepository hotelChainRepository) {
+        this.hotelChainRepository = hotelChainRepository;
+    }
+
+    @GetMapping("/info")
+    public String enumerateHotelChains(@RequestParam(name = "name", required = false) String chainName) {
+        if (chainName == null || chainName.isEmpty()) {
+            return new Gson().toJson(hotelChainRepository.findAll());
+        }
+        HotelChain rawResponse = hotelChainRepository.findHotelChainByChainName(chainName);
+        if (rawResponse == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return new Gson().toJson(rawResponse);
+    }
+}
--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/Spaghetti.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Spaghetti.java
@@ -1,30 +1,12 @@
 package ca.uottawa.csi2132.group196.spaghetti;
 
-import ca.uottawa.csi2132.group196.spaghetti.Repositories.HotelChainRepository;
-import com.google.gson.Gson;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Spaghetti {
-
-	private final HotelChainRepository hotelChainRepository;
-
-    public Spaghetti(final HotelChainRepository hotelChainRepository) {
-        this.hotelChainRepository = hotelChainRepository;
+    public static void main(String[] args) {
+        SpringApplication.run(Spaghetti.class, args);
     }
 
-    public static void main(String[] args) {
-		SpringApplication.run(Spaghetti.class, args);
-	}
-	
-	@GetMapping("/hotelChains")
-	public String enumerateHotelChains() {
-		return new Gson().toJson(hotelChainRepository.findAll());
-	}
 }
