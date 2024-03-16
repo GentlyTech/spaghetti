50dc34110e6ebec68fcb4747d931b25bb795c826
GentlyTech <andyyep@hotmail.com>
2024-03-16 16:48:36 -0400

    Add some more sample data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,7 @@ INSERT INTO hotel_chain_hotels (chain_name, hotels_hotel_id) SELECT 'Marriott', 
                                                                  
 WITH retVal AS (INSERT INTO hotel (hotel_name, rating) VALUES ('Sheraton Vancouver Wall Centre', 4) RETURNING hotel_id)
 INSERT INTO hotel_chain_hotels (chain_name, hotels_hotel_id) SELECT 'Marriott', hotel_id from retVal ON CONFLICT DO NOTHING;
+
+INSERT INTO hotel_chain_contacts (chain_name, name, email, phone_number) VALUES ('Marriott', 'Customer Service (US and canada)', '', '18006277468');
+INSERT INTO hotel_chain_contacts (chain_name, name, email, phone_number) VALUES ('Marriott', 'Customer Service (Australia)', '', '1800450010');
+INSERT INTO hotel_chain_contacts (chain_name, name, email, phone_number) VALUES ('Hilton', 'Customer Service', '', '18009974058');
