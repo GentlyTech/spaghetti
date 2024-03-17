e8639a5003312b51596406c95c0008c7ddec3228
GentlyTech <andyyep@hotmail.com>
2024-03-17 14:51:08 -0400

    Simplify sample SQL statements

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,11 +4,8 @@ INSERT INTO hotel_chain (chain_name) VALUES ('Marriott');
 INSERT INTO hotel_chain (chain_name) VALUES ('Travelodge');
 INSERT INTO hotel_chain (chain_name) VALUES ('Walt Disney');
 
-WITH retVal AS (INSERT INTO hotel (hotel_name, rating) VALUES ('Ottawa Marriott Hotel', 4) RETURNING hotel_id)
-INSERT INTO hotel_chain_hotels (chain_name, hotels_hotel_id) SELECT 'Marriott', hotel_id from retVal ON CONFLICT DO NOTHING;
-                                                                 
-WITH retVal AS (INSERT INTO hotel (hotel_name, rating) VALUES ('Sheraton Vancouver Wall Centre', 4) RETURNING hotel_id)
-INSERT INTO hotel_chain_hotels (chain_name, hotels_hotel_id) SELECT 'Marriott', hotel_id from retVal ON CONFLICT DO NOTHING;
+INSERT INTO hotel (hotel_name, owner, rating) VALUES ('Ottawa Marriott Hotel', 'Marriott', 4);
+INSERT INTO hotel (hotel_name, owner, rating) VALUES ('Sheraton Vancouver Wall Centre', 'Marriott', 4);
 
 INSERT INTO hotel_chain_contacts (chain_name, name, email, phone_number) VALUES ('Marriott', 'Customer Service (US and canada)', '', '18006277468');
 INSERT INTO hotel_chain_contacts (chain_name, name, email, phone_number) VALUES ('Marriott', 'Customer Service (Australia)', '', '1800450010');
