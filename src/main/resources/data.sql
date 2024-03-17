c78982fba75b8572fb401be6125b01045d6bdeac
GentlyTech <andyyep@hotmail.com>
2024-03-17 14:54:48 -0400

    Add some more sample data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,6 +6,7 @@ INSERT INTO hotel_chain (chain_name) VALUES ('Walt Disney');
 
 INSERT INTO hotel (hotel_name, owner, rating) VALUES ('Ottawa Marriott Hotel', 'Marriott', 4);
 INSERT INTO hotel (hotel_name, owner, rating) VALUES ('Sheraton Vancouver Wall Centre', 'Marriott', 4);
+INSERT INTO hotel (hotel_name, owner, rating) VALUES ('Hilton Garden Inn Ottawa Downtown', 'Hilton', 4);
 
 INSERT INTO hotel_chain_contacts (chain_name, name, email, phone_number) VALUES ('Marriott', 'Customer Service (US and canada)', '', '18006277468');
 INSERT INTO hotel_chain_contacts (chain_name, name, email, phone_number) VALUES ('Marriott', 'Customer Service (Australia)', '', '1800450010');
