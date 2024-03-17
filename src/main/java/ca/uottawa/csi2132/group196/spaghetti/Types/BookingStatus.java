5bee03d8a60f1a15cad6bcca355ac5dfaec38d61
GentlyTech <andyyep@hotmail.com>
2024-03-17 18:34:03 -0400

    Add warning

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/Types/BookingStatus.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Types/BookingStatus.java
@@ -1,5 +1,6 @@
 package ca.uottawa.csi2132.group196.spaghetti.Types;
 
+// Do not rearrange values once database has been populated. Otherwise, the status of any existing bopkings will be messed up.
 public enum BookingStatus {
     BOOKED,
     RENTED,
