c0fef20ab84ce030f9246491e99a73a333cbd22c
GentlyTech <andyyep@hotmail.com>
2024-03-17 23:22:14 -0400

    Add comment

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/Annotations/DoNotSerialize.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Annotations/DoNotSerialize.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Hints to the Gson serializer that the annotated field or type should be excluded from serialization.
+ * <p/>
+ * Note that for this to take effect, you need to use the GsonBuilder with addSerializationExclusionStrategy() and pass an instance of the DoNotSerializeStrategy into it.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.TYPE})
 public @interface DoNotSerialize {}
