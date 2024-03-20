0f342a8c00cba178dda41e8f49d5cd3e2c78fcc5
GentlyTech <andyyep@hotmail.com>
2024-03-20 13:09:26 -0400

    Remove Spring JPA Prepare for plain SQL via Spring JDBC

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/Controllers/HotelChainController.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Controllers/HotelChainController.java
@@ -1,43 +1,10 @@
 package ca.uottawa.csi2132.group196.spaghetti.Controllers;
 
-import ca.uottawa.csi2132.group196.spaghetti.Entities.HotelChain.HotelChain;
-import ca.uottawa.csi2132.group196.spaghetti.Repositories.HotelChainRepository;
-import ca.uottawa.csi2132.group196.spaghetti.Serialization.DoNotSerializeStrategy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/hotelChain")
 public class HotelChainController {
-
-    private final HotelChainRepository hotelChainRepository;
-
-    public HotelChainController(final HotelChainRepository hotelChainRepository) {
-        this.hotelChainRepository = hotelChainRepository;
-    }
-
-    @GetMapping("/info")
-    public String enumerateHotelChains(@RequestParam(name = "name", required = false) String chainName) {
-        Gson gson = new GsonBuilder().addSerializationExclusionStrategy(new DoNotSerializeStrategy()).create();
-        if (chainName == null || chainName.isEmpty()) {
-            return gson.toJson(hotelChainRepository.findAll());
-        }
-        HotelChain rawResponse = hotelChainRepository.getHotelChainByChainName(chainName);
-        if (rawResponse == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
-        return gson.toJson(rawResponse);
-    }
-
-    @GetMapping("/count")
-    public String getHotelCount(@RequestParam(name = "name") String chainName) {
-        int count = hotelChainRepository.getHotelCountByChainName(chainName);
-        return new Gson().toJson(count);
-    }
+    
 }
--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/Spaghetti.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Spaghetti.java
@@ -1,5 +1,6 @@
 package ca.uottawa.csi2132.group196.spaghetti;
 
+import jakarta.annotation.PreDestroy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,4 +10,11 @@ public class Spaghetti {
         SpringApplication.run(Spaghetti.class, args);
     }
 
+    /**
+     * Perform shutdown actions here.
+     */
+    @PreDestroy
+    public void onShutdown() {
+
+    }
 }
