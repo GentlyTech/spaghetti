7a5a00c98ceed4656842043a5507690eb159d160
GentlyTech <andyyep@hotmail.com>
2024-03-27 16:59:23 -0400

    Implement Address and Contact data classes

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DataClasses/Address.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DataClasses/Address.java
@@ -1,4 +1,58 @@
 package ca.uottawa.csi2132.group196.spaghetti.DataClasses;
 
 public class Address {
+    private int addressId = -1;
+    private String city = null;
+    private String country = null;
+    private String postal_code = null;
+    private String province = null;
+    private String street = null;
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
+    }
+
+    public void setPostal_code(String postal_code) {
+        this.postal_code = postal_code;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
 }
--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DataClasses/Contact.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DataClasses/Contact.java
@@ -1,4 +1,40 @@
 package ca.uottawa.csi2132.group196.spaghetti.DataClasses;
 
 public class Contact {
+    private int contactId = -1;
+    private String name = null;
+    private String email = null;
+    private String phone_number = null;
+
+    public int getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(int contactId) {
+        this.contactId = contactId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
 }
