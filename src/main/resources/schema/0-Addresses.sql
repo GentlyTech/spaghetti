b961e8dc8c0b12ff578531dcbfa9369682f9f2d2
GentlyTech <andyyep@hotmail.com>
2024-03-27 17:36:49 -0400

    Reorder fields

--- src/main/resources/schema/0-Addresses.sql
+++ src/main/resources/schema/0-Addresses.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS public.addresses
 (
     address_id serial NOT NULL,
-    city text,
-    country text,
-    postal_code text,
-    province text,
     street text,
+    city text,
+    province text,
+    postal_code text,
+    country text,
     CONSTRAINT addresses_pk PRIMARY KEY (address_id)
 );
