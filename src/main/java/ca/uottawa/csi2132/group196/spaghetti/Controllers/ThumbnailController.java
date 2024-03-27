72138d86fc3ef0cc5f2f4b88192a295c9aec44ef
GentlyTech <andyyep@hotmail.com>
2024-03-27 19:40:51 -0400

    Add placeholder ThumbnailController

---
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Controllers/ThumbnailController.java
@@ -0,0 +1,9 @@
+package ca.uottawa.csi2132.group196.spaghetti.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/thumbnail")
+public class ThumbnailController {
+}
