5e2e673f6723c594722e639a15d1a74bf8d599d6
GentlyTech <andyyep@hotmail.com>
2024-03-28 19:17:33 -0400

    Add DAO references

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/Controllers/HotelChainController.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Controllers/HotelChainController.java
@@ -1,5 +1,9 @@
 package ca.uottawa.csi2132.group196.spaghetti.Controllers;
 
+import ca.uottawa.csi2132.group196.spaghetti.DAOs.AddressDao;
+import ca.uottawa.csi2132.group196.spaghetti.DAOs.ContactDao;
+import ca.uottawa.csi2132.group196.spaghetti.DAOs.HotelChainDao;
+import ca.uottawa.csi2132.group196.spaghetti.DAOs.HotelDao;
 import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Address;
 import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Contact;
 import ca.uottawa.csi2132.group196.spaghetti.DataClasses.HotelChain;
@@ -25,13 +29,23 @@ public class HotelChainController {
     JdbcTemplate database;
     Gson serializer;
 
-    public HotelChainController(JdbcTemplate database, Gson serializer) {
+    AddressDao addressDao;
+    ContactDao contactDao;
+    HotelChainDao hotelChainDao;
+    HotelDao hotelDao;
+
+    public HotelChainController(JdbcTemplate database, Gson serializer, AddressDao addressDao, ContactDao contactDao, HotelChainDao hotelChainDao, HotelDao hotelDao) {
         this.database = database;
         this.serializer = serializer;
+        this.addressDao = addressDao;
+        this.contactDao = contactDao;
+        this.hotelChainDao = hotelChainDao;
+        this.hotelDao = hotelDao;
     }
 
     @GetMapping({"/info", "/info/"})
     public String getHotelChainInfoAll() {
+        // If we wanted to we could do something like `SELECT hotelChainInst.chain_name AS chain_name, COUNT(hotelInst.hotel_id) AS hotel_count FROM hotel_chain hotelChainInst FULL JOIN hotel hotelInst ON hotelChainInst.chain_name = hotelInst.owner GROUP BY hotelChainInst.chain_name;` but breaking the queries up is cleaner
         List<HotelChain> results = new HotelChainMapper(database.getDataSource(), "SELECT hotelChainInst.chain_name AS chain_name, COUNT(hotelInst.hotel_id) AS hotel_count FROM hotel_chain hotelChainInst FULL JOIN hotel hotelInst ON hotelChainInst.chain_name = hotelInst.owner GROUP BY hotelChainInst.chain_name;").execute();
         return serializer.toJson(results);
     }
@@ -41,21 +55,21 @@ public class HotelChainController {
         HotelChainMapper hotelChainMapper = new HotelChainMapper(database.getDataSource(), "SELECT hotelChainInst.chain_name AS chain_name, COUNT(hotelInst.hotel_id) AS hotel_count FROM hotel_chain hotelChainInst LEFT JOIN hotel hotelInst ON hotelChainInst.chain_name = hotelInst.owner WHERE chain_name = ? GROUP BY hotelChainInst.chain_name;");
         hotelChainMapper.declareParameter(new SqlParameterValue(Types.LONGVARCHAR, "chain_name"));
         HotelChain result = hotelChainMapper.findObject(chain_name);
-        
+
         if (result == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        
+
         ContactMapper contactMapper = new ContactMapper(database.getDataSource(), "SELECT contactInst.* FROM hotel_chain_contacts hotelChainInst LEFT JOIN contacts contactInst ON hotelChainInst.contact_id = contactInst.contact_id WHERE hotelChainInst.chain_name = ? GROUP BY contactInst.contact_id;");
         contactMapper.declareParameter(new SqlParameterValue(Types.LONGVARCHAR, "chain_name"));
         List<Contact> contacts = contactMapper.execute(chain_name);
-        
+
         result.setContacts(contacts);
 
         AddressMapper addressMapper = new AddressMapper(database.getDataSource(), "SELECT addressInst.* FROM hotel_chain_addresses hotelChainInst LEFT JOIN addresses addressInst ON hotelChainInst.address_id = addressInst.address_id WHERE hotelChainInst.chain_name = ? GROUP BY addressInst.address_id;");
         addressMapper.declareParameter(new SqlParameterValue(Types.LONGVARCHAR, "chain_name"));
         List<Address> addresses = addressMapper.execute(chain_name);
-        
+
         result.setAddresses(addresses);
-        
+
         return serializer.toJson(result);
     }
 }
