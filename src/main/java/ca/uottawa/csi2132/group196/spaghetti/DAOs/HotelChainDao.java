00fd9e16987676a32aa7a306e2aa29f1519fa81c
GentlyTech <andyyep@hotmail.com>
2024-03-28 14:39:32 -0400

    Implement HotelChainDao insertions

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/HotelChainDao.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/HotelChainDao.java
@@ -2,25 +2,20 @@ package ca.uottawa.csi2132.group196.spaghetti.DAOs;
 
 import ca.uottawa.csi2132.group196.spaghetti.DataClasses.HotelChain;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 @Repository
 public class HotelChainDao {
+    private final String INSERT_HOTEL_CHAIN_SQL = "INSERT INTO hotel_chain (chain_name) VALUES (?)";
     private final JdbcTemplate database;
-    private final SimpleJdbcInsert hotelChainInserter;
-    
+
     public HotelChainDao(DataSource dataSource) {
         database = new JdbcTemplate(dataSource);
-        hotelChainInserter = new SimpleJdbcInsert(dataSource);
     }
-    
-    public int insertHotelChain(HotelChain hotelChain) {
-        Map<String, String> returnColumns = new HashMap<>();
-        
+
+    public void insertHotelChain(HotelChain hotelChain) {
+        database.update(INSERT_HOTEL_CHAIN_SQL, hotelChain.getChainName());
     }
 }
