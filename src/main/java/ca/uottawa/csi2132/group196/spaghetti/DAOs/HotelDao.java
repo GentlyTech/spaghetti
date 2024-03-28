ca68fd12b292a46429d47275a9433db124ecd47c
GentlyTech <andyyep@hotmail.com>
2024-03-28 19:04:37 -0400

    Implement getHotelsByChainName, getHotelCountByChainName, and getHotelById

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/HotelDao.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/HotelDao.java
@@ -1,18 +1,24 @@
 package ca.uottawa.csi2132.group196.spaghetti.DAOs;
 
 import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Hotel;
+import ca.uottawa.csi2132.group196.spaghetti.Mappers.HotelMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.SqlParameterValue;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.sql.Types;
+import java.util.List;
 import java.util.Map;
 
 @Repository
 public class HotelDao {
     private final String INSERT_HOTEL_SQL = "INSERT INTO hotel (hotel_name, rating, owner) VALUES (?, ?, ?)";
+    private final String SELECT_HOTEL_BY_ID_SQL = "SELECT * FROM hotel WHERE hotel_id = ?";
+    private final String SELECT_HOTEL_BY_CHAIN_SQL = "SELECT * FROM hotel WHERE owner = ?";
     private final JdbcTemplate database;
 
     public HotelDao(JdbcTemplate database) {
@@ -36,8 +42,29 @@ public class HotelDao {
         if (key == null) return -1;
         return key.intValue();
     }
-    
+
+    public List<Hotel> getHotelsByChainName(String chainName) {
+        HotelMapper mapper = new HotelMapper(database.getDataSource(), SELECT_HOTEL_BY_CHAIN_SQL);
+        mapper.declareParameter(new SqlParameterValue(Types.LONGVARCHAR, "chain_name"));
+        return mapper.execute(chainName);
+    }
+
+    public int getHotelCountByChainName(String chainName) {
+        return getHotelsByChainName(chainName).size();
+    }
+
     public Hotel getHotelById(int hotelId) {
-        return null;
+        HotelMapper mapper = new HotelMapper(database.getDataSource(), SELECT_HOTEL_BY_ID_SQL);
+        mapper.declareParameter(new SqlParameterValue(Types.INTEGER, "hotel_id"));
+        return mapper.findObject(hotelId);
+    }
+
+    public Hotel getHotelById(String hotelId) {
+        try {
+            int parsedId = Integer.parseInt(hotelId);
+            return getHotelById(parsedId);
+        } catch (NumberFormatException ignored) {
+            return null;
+        }
     }
 }
