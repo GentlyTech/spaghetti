57a5cf087275dde00898a11b237da1f5a84d393f
GentlyTech <andyyep@hotmail.com>
2024-03-28 19:04:59 -0400

    Replace raw queries with DAOs

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/Controllers/HotelController.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Controllers/HotelController.java
@@ -1,18 +1,18 @@
 package ca.uottawa.csi2132.group196.spaghetti.Controllers;
 
+import ca.uottawa.csi2132.group196.spaghetti.DAOs.AddressDao;
+import ca.uottawa.csi2132.group196.spaghetti.DAOs.ContactDao;
+import ca.uottawa.csi2132.group196.spaghetti.DAOs.HotelDao;
 import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Hotel;
-import ca.uottawa.csi2132.group196.spaghetti.Mappers.HotelMapper;
 import com.google.gson.Gson;
 import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.SqlParameterValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.sql.Types;
 import java.util.List;
 
 @RestController
@@ -20,28 +20,34 @@ import java.util.List;
 public class HotelController {
     JdbcTemplate database;
     Gson serializer;
+    AddressDao addressDao;
+    ContactDao contactDao;
+    HotelDao hotelDao;
 
-    public HotelController(JdbcTemplate database, Gson serializer) {
+    public HotelController(JdbcTemplate database, Gson serializer, AddressDao addressDao, ContactDao contactDao, HotelDao hotelDao) {
         this.database = database;
         this.serializer = serializer;
+        this.addressDao = addressDao;
+        this.contactDao = contactDao;
+        this.hotelDao = hotelDao;
     }
 
     @GetMapping({"/info/byChain/{chain_name}", "/info/byChain/{chain_name}/"})
     public String getHotelInfoByHotelChain(@PathVariable String chain_name) {
-        HotelMapper mapper = new HotelMapper(database.getDataSource(), "SELECT * FROM hotel WHERE owner = ?");
-        mapper.declareParameter(new SqlParameterValue(Types.LONGVARCHAR, "chain_name"));
-        List<Hotel> results = mapper.execute(chain_name);
+        List<Hotel> results = hotelDao.getHotelsByChainName(chain_name);
+        // TODO get contacts
+        // TODO get addresses
         if (results.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         return serializer.toJson(results);
     }
 
     @GetMapping({"/info/byId/{hotel_id}", "/info/byId/{hotel_id}/"})
     public String getHotelInfoByHotelId(@PathVariable String hotel_id) {
-        HotelMapper mapper = new HotelMapper(database.getDataSource(), "SELECT * FROM hotel WHERE hotel_id = ?");
-        mapper.declareParameter(new SqlParameterValue(Types.INTEGER, "hotel_id"));
-        List<Hotel> results = mapper.execute(hotel_id);
-        if (results.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        return serializer.toJson(results);
+        Hotel result = hotelDao.getHotelById(hotel_id);
+        // TODO get contacts
+        // TODO get addresses
+        if (result == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return serializer.toJson(result);
     }
 
     public String getHotelIdsByHotelName() {
