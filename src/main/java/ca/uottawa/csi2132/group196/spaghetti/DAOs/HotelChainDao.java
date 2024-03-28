f35da8c8dab04573bc23ddfc76f3fd9fb66847f2
GentlyTech <andyyep@hotmail.com>
2024-03-28 16:17:01 -0400

    Replace DataSource with JdbcTemplate Add getHotelChainById placeholder

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/HotelChainDao.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/HotelChainDao.java
@@ -1,5 +1,6 @@
 package ca.uottawa.csi2132.group196.spaghetti.DAOs;
 
+import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Hotel;
 import ca.uottawa.csi2132.group196.spaghetti.DataClasses.HotelChain;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -11,11 +12,15 @@ public class HotelChainDao {
     private final String INSERT_HOTEL_CHAIN_SQL = "INSERT INTO hotel_chain (chain_name) VALUES (?)";
     private final JdbcTemplate database;
 
-    public HotelChainDao(DataSource dataSource) {
-        database = new JdbcTemplate(dataSource);
+    public HotelChainDao(JdbcTemplate database) {
+        this.database = database;
     }
 
     public void insertHotelChain(HotelChain hotelChain) {
         database.update(INSERT_HOTEL_CHAIN_SQL, hotelChain.getChainName());
+    }
+
+    public HotelChain getHotelChainById(String chainName) {
+        return null;
     }
 }
