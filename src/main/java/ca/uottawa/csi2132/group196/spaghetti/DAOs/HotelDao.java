0f8d3bf237938a66fc6c0fd3867192414c53c860
GentlyTech <andyyep@hotmail.com>
2024-03-28 14:39:36 -0400

    Implement HotelDao insertions

---
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/HotelDao.java
@@ -0,0 +1,41 @@
+package ca.uottawa.csi2132.group196.spaghetti.DAOs;
+
+import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Hotel;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Map;
+import java.util.Objects;
+
+@Repository
+public class HotelDao {
+    private final String INSERT_HOTEL_SQL = "INSERT INTO hotel (hotel_name, rating, owner) VALUES (?, ?, ?)";
+    private final JdbcTemplate database;
+
+    public HotelDao(DataSource dataSource) {
+        database = new JdbcTemplate(dataSource);
+    }
+
+    public int insertHotel(Hotel hotel) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        database.update(connection -> {
+            PreparedStatement statement = connection.prepareStatement(INSERT_HOTEL_SQL, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, hotel.getHotelName());
+            statement.setInt(2, hotel.getRating());
+            statement.setString(3, hotel.getOwner());
+            return statement;
+        }, keyHolder);
+        
+        Map<String, Object> keys = keyHolder.getKeys();
+        if (keys == null) return -1;
+        Number key = (Number) keys.get("hotel_id");
+        if (key == null) return -1;
+        return key.intValue();
+    }
+}
