98469295d4c87d2640426d5219942bfb4af3f7c0
GentlyTech <andyyep@hotmail.com>
2024-03-28 01:29:04 -0400

    DAO stuff

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/Spaghetti.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Spaghetti.java
@@ -1,19 +1,52 @@
 package ca.uottawa.csi2132.group196.spaghetti;
 
+import ca.uottawa.csi2132.group196.spaghetti.DataClasses.HotelChain;
+import com.google.gson.Gson;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.io.*;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class Spaghetti {
+    JdbcTemplate database;
+    Gson serializer;
+
+    public Spaghetti(JdbcTemplate database, Gson serializer) {
+        this.database = database;
+        this.serializer = serializer;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Spaghetti.class, args);
     }
 
+    /**
+     * Perform init actions here.
+     */
     @PostConstruct
     public void onStartup() {
-        
+        InputStream inputStream;
+        Reader reader;
+
+        // Init HotelChains
+        try {
+            inputStream = new ClassPathResource("sampleData/HotelChains.json").getInputStream();
+            reader = new InputStreamReader(inputStream);
+            HotelChain[] hotelChains = serializer.fromJson(reader, HotelChain[].class);
+            for (HotelChain hotelChain : hotelChains) {
+                
+            }
+        }
+        catch (IOException ignored) {
+            
+        }
+
     }
 
     /**
