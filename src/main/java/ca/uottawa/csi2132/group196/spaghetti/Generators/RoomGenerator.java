df6a767130440a1a7152b0b40080f55e61756cfc
GentlyTech <andyyep@hotmail.com>
2024-03-29 16:10:11 -0400

    Fix some stuff

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/Generators/RoomGenerator.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Generators/RoomGenerator.java
@@ -1,9 +1,8 @@
 package ca.uottawa.csi2132.group196.spaghetti.Generators;
 
-import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Room;
+import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Amenity;
 import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Hotel;
-
-import ca.uottawa.csi2132.group196.spaghetti.Gson.CustomSerializer;
+import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Room;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -11,69 +10,67 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RoomGenerator {
-    public static final int NUM_ROOMS = 100;
-    public static void main(String args) {
+    private final Hotel hotel;
+    private final Amenity amenities;
+
+    public RoomGenerator(Hotel hotel) {
+        this.hotel = hotel;
+        this.amenities = null;
+    }
+
+    public RoomGenerator(Hotel hotel, Amenity amenities) {
+        this.hotel = hotel;
+        this.amenities = amenities;
+    }
+
+    public List<Room> generateRooms(int numRooms) {
+        Random rand = new Random();
         List<Room> rooms = new LinkedList<>();
 
-        for (int i = 0; i < NUM_ROOMS; i++) {
+        for (int i = 0; i < numRooms; i++) {
             Room room = new Room();
             room.setRoomNumber(i + 1);
 
             //extendable
-            int int_random = rand.nextInt(1); 
-            if (int_random == 1){
-                room.setExtendable(true);
-            } else {
-                room.setExtendable(false);
-            }
+            int int_random = rand.nextInt(2);
+            room.setExtendable(int_random == 1);
 
             //capacity
-            int int_random2 = rand.nextInt(9); 
-            room.setCapacity(int_random2+1);
+            int capacity = rand.nextInt(10);
+            room.setCapacity(capacity);
 
             //price based on capacity
             if (hotel.getRating() == 1) {
                 int int_random4 = ThreadLocalRandom.current().nextInt(100.00, 150.00);
-                room.setPrice(int_random4+int_random4*.50*capacity);
+                room.setPrice(int_random4 + int_random4 * .50 * capacity);
             } else if (hotel.getRating() == 2) {
                 int int_random4 = ThreadLocalRandom.current().nextInt(150.00, 200.00);
-                room.setPrice(int_random4+int_random4*.50*capacity);
+                room.setPrice(int_random4 + int_random4 * .50 * capacity);
             } else if (hotel.getRating() == 3) {
                 int int_random4 = ThreadLocalRandom.current().nextInt(200.00, 250.00);
-                room.setPrice(inroot_random4+int_random4*.50*capacity);
+                room.setPrice(inroot_random4 + int_random4 * .50 * capacity);
             } else if (hotel.getRating() == 4) {
                 int int_random4 = ThreadLocalRandom.current().nextInt(250.00, 400.00);
-                room.setPrice(int_random4+int_random4*.50*capacity);
+                room.setPrice(int_random4 + int_random4 * .50 * capacity);
             } else {
                 int int_random4 = ThreadLocalRandom.current().nextInt(400.00, 1000.00);
-                room.setPrice(int_random4+int_random4*.50*capacity);
+                room.setPrice(int_random4 + int_random4 * .50 * capacity);
             }
 
             //viewtype
-            int int_random3 = rand.nextInt(1);
-            if (int_random == 1){
+            int int_random3 = rand.nextInt(2);
+            if (int_random == 1) {
                 room.setViewType("Sea View");
             } else {
                 room.setViewType("Mountain View");
             }
- 
+
+            if (amenities == null) return rooms;
+
             //amenities
-            //code for randomly choose from list is from Baeldung
-            Random rand = new Random();
-            List<Amenities> baseList = Lists.newArrayList("TV", "Air Conditioner", "Fridge", "Jacuzzi");
-            List<Amenities> amenities = new List<Amenities>();
-            int numberOfElements = rand.nextInt(baselist.size());;
+            room.setAmenities(null); // TODO pick amenities
 
-            for (int j = 0; j < numberOfElements; i++) {
-                int randomIndex = rand.nextInt(givenList.size());
-                String randomElement = givenList.get(randomIndex);
-                givenList.remove(randomIndex); //removes element for no repeats
-                amenities.add(randomElement);
-            }
-            room.setAmenities(amenities);
-
+            return rooms;
         }
-
-        System.out.println(CustomSerializer.getCustomSerializer().toJson(rooms));
     }
 }
