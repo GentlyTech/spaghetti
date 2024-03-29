27bc34f8dfa686260e838a8217714e5710d8752f
GentlyTech <andyyep@hotmail.com>
2024-03-29 15:18:55 -0400

    Add some placeholders for CustomerDao

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/CustomerDao.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/CustomerDao.java
@@ -1,5 +1,6 @@
 package ca.uottawa.csi2132.group196.spaghetti.DAOs;
 
+import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Customer;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,13 @@ public class CustomerDao {
 
     public CustomerDao(JdbcTemplate database) {
         this.database = database;
+    }
+
+    public void insertCustomer(Customer customer) {
+        return;
+    }
+
+    public Customer getCustomerById(int customerId) {
+        return null;
     }
 }
