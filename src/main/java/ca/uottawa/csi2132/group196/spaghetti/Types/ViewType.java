a846b0b07052d0361f25d0e4b7d015b41232dd2f
GentlyTech <andyyep@hotmail.com>
2024-03-29 12:23:10 -0400

    Update ViewType enum

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/Types/ViewType.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Types/ViewType.java
@@ -1,7 +1,23 @@
 package ca.uottawa.csi2132.group196.spaghetti.Types;
 
 public enum ViewType {
-    BORING,
-    OCEAN,
-    MOUNTAIN
+    BORING(0),
+    OCEAN(1),
+    MOUNTAIN(2);
+
+    private final int value;
+    ViewType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "ViewType{" +
+                "value=" + value +
+                '}';
+    }
 }
