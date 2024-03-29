1ab365e3b95ddb77d57fe234d831164274408b98
aiden <69060711+Aiden-Garrett@users.noreply.github.com>
2024-03-29 13:58:45 -0400

    booking dao

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/BookingDao.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/BookingDao.java
@@ -2,11 +2,22 @@ package ca.uottawa.csi2132.group196.spaghetti.DAOs;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.sql.Date;
+
 public class BookingDao {
+
+    int room_number;
+    int customer_id;
+    int hotel_id;
+    String booking_status;
+
+    Date check_in_date;
+    Date check_out_date;
+    double damage_fee;
+
     private final JdbcTemplate database;
 
     public BookingDao(JdbcTemplate database) {
         this.database = database;
     }
-
 }
