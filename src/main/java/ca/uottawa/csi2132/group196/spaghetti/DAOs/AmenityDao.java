d1b71c91d92d0206dd6889298bcc12cfc7ab23d2
GentlyTech <andyyep@hotmail.com>
2024-03-29 00:49:47 -0400

    Add placeholders

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/AmenityDao.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/AmenityDao.java
@@ -1,5 +1,7 @@
 package ca.uottawa.csi2132.group196.spaghetti.DAOs;
 
+import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Hotel;
+import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Room;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class AmenityDao {
@@ -7,5 +9,13 @@ public class AmenityDao {
 
     public AmenityDao(JdbcTemplate database) {
         this.database = database;
+    }
+
+    public void insertAmenitiesFromHotel(Hotel hotel) {
+
+    }
+
+    public void insertAmenitiesFromRoom(Room room) {
+
     }
 }
