e3671db8834ea763e9c8f193388ec2c516c95da6
GentlyTech <andyyep@hotmail.com>
2024-03-29 00:53:13 -0400

    Implement Room and Problem data classes

---
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DataClasses/Problem.java
@@ -0,0 +1,22 @@
+package ca.uottawa.csi2132.group196.spaghetti.DataClasses;
+
+public class Problem {
+    private String caption;
+    private String description;
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DataClasses/Room.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DataClasses/Room.java
@@ -1,4 +1,89 @@
 package ca.uottawa.csi2132.group196.spaghetti.DataClasses;
 
+import ca.uottawa.csi2132.group196.spaghetti.Types.ViewType;
+
+import java.util.List;
+
 public class Room {
+    private int hotelId;
+    private int roomNumber;
+    private double price;
+    private double damageFee;
+    private boolean extendable;
+    private String occupancyType;
+    private ViewType viewType;
+    private List<Amenity> amenities;
+    private List<Problem> problems;
+
+    public List<Amenity> getAmenities() {
+        return amenities;
+    }
+
+    public void setAmenities(List<Amenity> amenities) {
+        this.amenities = amenities;
+    }
+
+    public List<Problem> getProblems() {
+        return problems;
+    }
+
+    public void setProblems(List<Problem> problems) {
+        this.problems = problems;
+    }
+
+    public int getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(int hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getDamageFee() {
+        return damageFee;
+    }
+
+    public void setDamageFee(double damageFee) {
+        this.damageFee = damageFee;
+    }
+
+    public boolean isExtendable() {
+        return extendable;
+    }
+
+    public void setExtendable(boolean extendable) {
+        this.extendable = extendable;
+    }
+
+    public String getOccupancyType() {
+        return occupancyType;
+    }
+
+    public void setOccupancyType(String occupancyType) {
+        this.occupancyType = occupancyType;
+    }
+
+    public ViewType getViewType() {
+        return viewType;
+    }
+
+    public void setViewType(ViewType viewType) {
+        this.viewType = viewType;
+    }
 }
