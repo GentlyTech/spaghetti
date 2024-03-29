9fd8e04b79cd0c105a286bf4c79ae5df3c8f7920
GentlyTech <andyyep@hotmail.com>
2024-03-29 00:08:19 -0400

    Implement Amenity and Role data classes

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DataClasses/Amenity.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DataClasses/Amenity.java
@@ -1,5 +1,22 @@
 package ca.uottawa.csi2132.group196.spaghetti.DataClasses;
 
 public class Amenity {
-    
+    private String name = null;
+    private String description = null;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
