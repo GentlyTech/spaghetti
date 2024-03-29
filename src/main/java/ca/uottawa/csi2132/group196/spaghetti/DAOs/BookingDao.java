6d82bf88e5666bbee3ecabf4d2f1e03addf83c86
GentlyTech <andyyep@hotmail.com>
2024-03-29 15:07:25 -0400

    Add missing @Repository declaration

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/BookingDao.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/BookingDao.java
@@ -1,7 +1,9 @@
 package ca.uottawa.csi2132.group196.spaghetti.DAOs;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class BookingDao {
     private final JdbcTemplate database;
 
