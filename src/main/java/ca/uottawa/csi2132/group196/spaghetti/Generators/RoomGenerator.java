50122ddce8561fd6646392bfd7f7d08d154aac6d
GentlyTech <andyyep@hotmail.com>
2024-03-29 13:44:17 -0400

    Add RoomGenerator template

---
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Generators/RoomGenerator.java
@@ -0,0 +1,20 @@
+package ca.uottawa.csi2132.group196.spaghetti.Generators;
+
+import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Room;
+import ca.uottawa.csi2132.group196.spaghetti.Gson.CustomSerializer;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class RoomGenerator {
+    public static final int NUM_ROOMS = 100;
+    public static void main(String args) {
+        List<Room> rooms = new LinkedList<>();
+
+        for (int i = 0; i < NUM_ROOMS; i++) {
+
+        }
+
+        System.out.println(CustomSerializer.getCustomSerializer().toJson(rooms));
+    }
+}
