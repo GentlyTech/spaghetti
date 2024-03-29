b8a26151b13e63236503fabefa40db94234d8b9f
GentlyTech <andyyep@hotmail.com>
2024-03-29 10:39:55 -0400

    Work on AmenityDao

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/AmenityDao.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/AmenityDao.java
@@ -1,10 +1,16 @@
 package ca.uottawa.csi2132.group196.spaghetti.DAOs;
 
+import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Amenity;
 import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Hotel;
 import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Room;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.util.List;
+
 public class AmenityDao {
+    private static final String INSERT_AMENITY_HOTEL_SQL = "INSERT INTO hotel_amenities (hotel_id, name, description) VALUES (?, ?, ?)";
+    private static final String INSERT_AMENITY_ROOM_SQL = "INSERT INTO room_amenities (hotel_id, room_number, name, description) VALUES (?, ?, ?, ?, ?, ?)";
+
     private final JdbcTemplate database;
 
     public AmenityDao(JdbcTemplate database) {
@@ -12,7 +18,7 @@ public class AmenityDao {
     }
 
     public void insertAmenitiesFromHotel(Hotel hotel) {
-
+        List<Amenity> amenities = hotel.getAmenities();
     }
 
     public void insertAmenitiesFromRoom(Room room) {
