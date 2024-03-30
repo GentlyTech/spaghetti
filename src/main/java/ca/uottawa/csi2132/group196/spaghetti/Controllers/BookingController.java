6925e879e326056e67d490bc5f0365abf6a5205f
GentlyTech <andyyep@hotmail.com>
2024-03-30 13:30:06 -0400

    Add placeholder BookingController

---
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Controllers/BookingController.java
@@ -0,0 +1,10 @@
+package ca.uottawa.csi2132.group196.spaghetti.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/booking")
+public class BookingController {
+    
+}
