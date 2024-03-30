812ddd55d96a7b7f4a6a8f2c4c17b4aeb3567a12
GentlyTech <andyyep@hotmail.com>
2024-03-30 13:30:28 -0400

    Implement query

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/BookingDao.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/BookingDao.java
@@ -8,20 +8,16 @@ import ca.uottawa.csi2132.group196.spaghetti.Utils.FieldMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.SqlParameterValue;
+import org.springframework.stereotype.Repository;
 
-import java.awt.print.Book;
-import java.sql.Connection;
 import java.sql.Date;
 import java.sql.Types;
 import java.util.List;
-import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookingDao {
 
 
-
-    private final JdbcTemplate database;
     private static final String SELECT_BOOKING_BY_HOTEL_ROOM_SQL = "SELECT * FROM booking WHERE room_id = ? AND hotel_id = ?";
     private static final String SELECT_BOOKINGS_BY_CUSTOMER_SQL = "SELECT * FROM booking WHERE customer_id = ?";
     private static final String SELECT_BOOKING_BY_CUSTOMER_HOTEL_SQL = "SELECT * FROM booking WHERE customer_id = ? AND hotel_id = ?";
@@ -29,6 +25,7 @@ public class BookingDao {
     private static final String UPDATE_BOOKING_STATUS_SQL = "UPDATE booking SET booking_status = ? WHERE hotel_id = ? AND customer_id = ? AND room_number = ?";
     private static final String SELECT_BOOKINGS_BY_HOTEL_ROOM = "SELECT * FROM booking WHERE booking.hotel_id = ? AND booking.room_number = ?";
     private static final String BOOKING_EXISTS_COUNT_SQL = "SELECT COUNT(*) FROM booking WHERE (booking.hotel_id = ? AND booking.room_number = ?) AND EXISTS(SELECT * FROM booking WHERE check_in_date > ? AND check_out_date > ?)";
+    private final JdbcTemplate database;
 
 
     public BookingDao(JdbcTemplate database) {
@@ -37,16 +34,16 @@ public class BookingDao {
 
 
     public void insertBooking(Booking booking) {
-        database.update(INSERT_BOOKING_SQL, booking.getRoomNumber()  , booking.getCustomerId(), booking.getHotelId(), booking.getBookingStatus(), booking.getCheckInDate(), booking.getCheckOutDate(), booking.getDamageFee());
+        database.update(INSERT_BOOKING_SQL, booking.getRoomNumber(), booking.getCustomerId(), booking.getHotelId(), booking.getBookingStatus(), booking.getCheckInDate(), booking.getCheckOutDate(), booking.getDamageFee());
     }
 
-    public List<Booking> getBookingsByCustomer(Customer customer){
+    public List<Booking> getBookingsByCustomer(Customer customer) {
         FieldMapper<Booking> mapper = new FieldMapper<>(database.getDataSource(), SELECT_BOOKINGS_BY_CUSTOMER_SQL, Booking.class);
         mapper.declareParameter(new SqlParameterValue(Types.INTEGER, "customer_id"));
         return mapper.execute(customer.getCustomerId());
     }
 
-    public List<Booking> getBookingsByCustomerAndHotel(Customer customer, Hotel hotel){
+    public List<Booking> getBookingsByCustomerAndHotel(Customer customer, Hotel hotel) {
         FieldMapper<Booking> mapper = new FieldMapper<>(database.getDataSource(), SELECT_BOOKING_BY_CUSTOMER_HOTEL_SQL, Booking.class);
         mapper.declareParameter(new SqlParameterValue(Types.INTEGER, "customer_id"));
         mapper.declareParameter(new SqlParameter(Types.INTEGER, "hotel_id"));
@@ -57,7 +54,7 @@ public class BookingDao {
         database.update(UPDATE_BOOKING_STATUS_SQL, newStatus, booking.getHotelId(), booking.getCustomerId(), booking.getCustomerId());
     }
 
-    public Boolean isBooked(Date checkIn, Date checkOut, Hotel hotel, Room room) {
+    public Boolean isBooked(Date checkInDate, Date checkOutDate, Room room) {
 
         Date today = new Date(System.currentTimeMillis());
 
@@ -70,10 +67,13 @@ public class BookingDao {
 
         //String sql = STR."SELECT COUNT(*) FROM booking WHERE (booking.hotel_id =  \{hotel.getHotelId()} + AND booking.room_number = \{room.getRoomNumber()}) AND EXISTS(SELECT * FROM booking WHERE check_in_date > \{today} AND check_out_date > \{today})";
 
-//        return database.execute(sql) != 0;
+        // return database.execute(sql) != 0;
 
         //TODO: FIGURE OUT HOW TO RUN AGGREGATE QUERY.
-        return false;
+
+        Integer bookingCount = database.queryForObject(BOOKING_EXISTS_COUNT_SQL, Integer.class, room.getHotelId(), room.getRoomNumber(), checkInDate, checkOutDate);
+        if (bookingCount == null) return false;
+        return bookingCount > 0;
     }
 
 
