18958fd3eb44d6b623791f44c853cb39db24df58
GentlyTech <andyyep@hotmail.com>
2024-03-31 18:57:20 -0400

    Fix error

--- src/main/resources/triggers.sql
+++ src/main/resources/triggers.sql
@@ -1,9 +1,9 @@
 CREATE FUNCTION delete_hotel() RETURNS TRIGGER AS
-$$
+'
 BEGIN
     DELETE FROM room WHERE public.room.hotel_id = old.hotel_id;
-END
-$$;
+END;
+' LANGUAGE plpgsql;
 
 CREATE TRIGGER on_hotel_deletion
     BEFORE DELETE
