002332b6c9a84add1bcd35b89a4647c2ce5d8ba2
aiden <69060711+Aiden-Garrett@users.noreply.github.com>
2024-03-31 00:11:01 -0400

    added controlers

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/Controllers/CustomerController.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Controllers/CustomerController.java
@@ -1,10 +1,24 @@
 package ca.uottawa.csi2132.group196.spaghetti.Controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ca.uottawa.csi2132.group196.spaghetti.DAOs.*;
+import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Customer;
+import com.google.gson.Gson;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
-    
+
+    JdbcTemplate database;
+    Gson serializer;
+    CustomerDao customerDao;
+
+
+
+    public CustomerController(JdbcTemplate database, Gson serializer, CustomerDao customerDao) {
+        this.database = database;
+        this.serializer = serializer;
+        this.customerDao = customerDao;
+    }
 }
---
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Controllers/RoomControler.java
@@ -0,0 +1,30 @@
+package ca.uottawa.csi2132.group196.spaghetti.Controllers;
+
+import ca.uottawa.csi2132.group196.spaghetti.DAOs.AddressDao;
+import ca.uottawa.csi2132.group196.spaghetti.DAOs.ContactDao;
+import ca.uottawa.csi2132.group196.spaghetti.DAOs.RoomDao;
+import com.google.gson.Gson;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/room")
+public class RoomControler {
+    JdbcTemplate database;
+    Gson serializer;
+    RoomDao roomDao;
+
+    public RoomControler(JdbcTemplate database, Gson serializer, RoomDao roomDao) {
+        this.database = database;
+        this.serializer = serializer;
+        this.roomDao = roomDao;
+    }
+
+    @GetMapping("/info/{hotelId}")
+    public String getRoomsInHotel(int hotelId){
+        return serializer.toJson(roomDao.getRoomsByHotel(hotelId));
+    }
+
+}
