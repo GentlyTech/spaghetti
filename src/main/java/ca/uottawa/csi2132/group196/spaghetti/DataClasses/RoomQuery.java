9194cf6f86b38595efa32a661236e3cd7d23820b
GentlyTech <andyyep@hotmail.com>
2024-04-01 15:24:31 -0400

    Modify RoomQuery to allow null values on primitives

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DataClasses/RoomQuery.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DataClasses/RoomQuery.java
@@ -1,20 +1,33 @@
 package ca.uottawa.csi2132.group196.spaghetti.DataClasses;
 
-public class RoomQuery {
-    private double price;
-    private String chainName;
-    private String checkInDate;
-    private String checkOutDate;
-    private String hotelName;
-    private String location;
-    private int rating;
-    private int capacity;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
-    public double getPrice() {
+public class RoomQuery {
+    private Double price = -1.0;
+    
+    private String chainName = null;
+    
+    private String checkInDate = null;
+    
+    private String checkOutDate = null;
+    
+    private String hotelName = null;
+    
+    private String location = null;
+    
+    private Integer rating = -1;
+    
+    private Integer capacity = -1;
+
+    public Double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
+    public void setPrice(Double price) {
+        if (price == null) {
+            this.price = -1.0;
+            return;
+        }
         this.price = price;
     }
 
@@ -58,19 +71,27 @@ public class RoomQuery {
         this.location = location;
     }
 
-    public int getRating() {
+    public Integer getRating() {
         return rating;
     }
 
-    public void setRating(int rating) {
+    public void setRating(Integer rating) {
+        if (rating == null) {
+            this.rating = -1;
+            return;
+        }
         this.rating = rating;
     }
 
-    public int getCapacity() {
+    public Integer getCapacity() {
         return capacity;
     }
 
-    public void setCapacity(int capacity) {
+    public void setCapacity(Integer capacity) {
+        if (capacity == null) {
+            this.capacity = -1;
+            return;
+        }
         this.capacity = capacity;
     }
 }
