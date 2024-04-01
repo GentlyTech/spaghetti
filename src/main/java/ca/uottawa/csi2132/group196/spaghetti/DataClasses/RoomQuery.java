e2240771a851fd243d08df919e08720cf35fc19c
GentlyTech <andyyep@hotmail.com>
2024-04-01 00:56:00 -0400

    Add RoomQuery data class

---
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DataClasses/RoomQuery.java
@@ -0,0 +1,76 @@
+package ca.uottawa.csi2132.group196.spaghetti.DataClasses;
+
+public class RoomQuery {
+    double price;
+    private String chainName;
+    private String checkInDate;
+    private String checkOutDate;
+    private String hotelName;
+    private String location;
+    private String rating;
+    private int capacity;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getChainName() {
+        return chainName;
+    }
+
+    public void setChainName(String chainName) {
+        this.chainName = chainName;
+    }
+
+    public String getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(String checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public String getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(String checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
