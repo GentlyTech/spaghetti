1de9f211ec8f1e2f2c47939233886a6d27411422
GentlyTech <andyyep@hotmail.com>
2024-03-31 22:11:27 -0400

    Implement getEmployee

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/EmployeeDao.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/DAOs/EmployeeDao.java
@@ -1,18 +1,22 @@
 package ca.uottawa.csi2132.group196.spaghetti.DAOs;
 
 import ca.uottawa.csi2132.group196.spaghetti.DataClasses.Employee;
+import ca.uottawa.csi2132.group196.spaghetti.Utils.FieldMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.SqlParameterValue;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.Map;
 
 @Repository
 public class EmployeeDao {
     private static final String INSERT_EMPLOYEE_SQL = "INSERT INTO employee (name, sin) VALUES (?, ?)";
+    private static final String SELECT_EMPLOYEE_SQL = "SELECT * FROM employee WHERE employee_id = ?";
 
     private final JdbcTemplate database;
 
@@ -34,5 +38,11 @@ public class EmployeeDao {
         Number employeeId = (Number) keys.get("employee_id");
         if (employeeId == null) return -1;
         return employeeId.intValue();
+    }
+    
+    public Employee getEmployee(int employeeId) {
+        FieldMapper<Employee> mapper = new FieldMapper<>(database.getDataSource(), SELECT_EMPLOYEE_SQL, Employee.class);
+        mapper.declareParameter(new SqlParameterValue(Types.INTEGER, "employee_id"));
+        return mapper.findObject(employeeId);
     }
 }
