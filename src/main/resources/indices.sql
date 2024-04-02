dcd4264bd343c66c7537e4d2e6136d7b371e5342
aiden <69060711+Aiden-Garrett@users.noreply.github.com>
2024-04-02 17:57:52 -0400

    modified indices to attempt to speed up queries

--- src/main/resources/indices.sql
+++ src/main/resources/indices.sql
@@ -1,3 +1,4 @@
-CREATE INDEX hotel_index on hotel USING hash(hotel_id);
-CREATE INDEX room_index ON room (hotel_id, room_number);
+CREATE INDEX hotel_index on hotel (hotel_id, rating);
+CREATE INDEX room_index ON room (hotel_id, room_number, price, capacity);
 CREATE INDEX booking_index ON booking (room_number, customer_id, hotel_id);
+CREATE INDEX addresses_index ON addresses (city, country, province);
