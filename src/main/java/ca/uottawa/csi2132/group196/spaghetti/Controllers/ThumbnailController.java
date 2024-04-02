29fd59096afc688ac1aa43a2c289a01f07237c0e
GentlyTech <andyyep@hotmail.com>
2024-04-02 14:19:22 -0400

    Fix mimeType

--- src/main/java/ca/uottawa/csi2132/group196/spaghetti/Controllers/ThumbnailController.java
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Controllers/ThumbnailController.java
@@ -4,32 +4,29 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/thumbnail")
 public class ThumbnailController {
     private static final String DEFAULT_HOTEL_THUMBNAIL_PATH = "/thumbnails/hotels/default.png";
-    
+
     private Logger logger;
-    
+
     public ThumbnailController() {
         this.logger = Logger.getLogger(this.getClass().getSimpleName());
     }
-    
-    @GetMapping("/hotel")
+
+    @GetMapping(value = "/hotel", produces = MediaType.IMAGE_PNG_VALUE)
     public @ResponseBody Resource getGenericThumbnailForHotel() {
         Resource handle = new ClassPathResource(DEFAULT_HOTEL_THUMBNAIL_PATH);
         if (!handle.exists()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        
+
         try {
             return new ByteArrayResource(handle.getContentAsByteArray());
         } catch (IOException e) {
@@ -37,8 +34,8 @@ public class ThumbnailController {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    
-    @GetMapping("/hotel/{hotelId}")
+
+    @GetMapping(value = "/hotel/{hotelId}", produces = MediaType.IMAGE_PNG_VALUE)
     public @ResponseBody Resource getThumbnailForHotel(@PathVariable String hotelId) {
         // TODO consider mapping hotelIds to thumbnails
         return getGenericThumbnailForHotel();
