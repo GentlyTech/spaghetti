36797de56ecaa84c4eb1c5d53c5ef35ffcfb33bf
GentlyTech <andyyep@hotmail.com>
2024-04-02 15:40:07 -0400

    Restore BookingStatus

---
+++ src/main/java/ca/uottawa/csi2132/group196/spaghetti/Enums/BookingStatus.java
@@ -0,0 +1,9 @@
+package ca.uottawa.csi2132.group196.spaghetti.Enums;
+
+// Do not rearrange values once database has been populated. Otherwise, the status of any existing bopkings will be messed up.
+public enum BookingStatus {
+    BOOKED,
+    RENTED,
+    COMPLETED,
+    CANCELLED
+}
